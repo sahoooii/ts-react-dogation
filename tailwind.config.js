86112e07e8e924a17d4cdd7b4aa560fcd4b502ef
sahoooii <saho.chu@gmail.com>
2023-04-03 15:43:30 +0900

    init setting

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				'light-green-20': '#E6FFE9',
+				'light-green-50': '#B1F9D0',
+				'light-green-100': '#96D0CD',
+				'light-blue-20': '#BAD3FF',
+				'light-blue-50': '#65CBDA',
+				'light-blue-100': '#15B2D1',
+				'dark-blue-50': '#236E95',
+				'dark-blue-100': '#1D5171',
+				'sand-50': '#FEDBB7',
+				'sand-80': '#DFCE9D',
+			},
+			backgroundImage: (theme) => ({
+				'gradient-yellowRed': 'linear-gradient(to right, #36d1dc, #5b86e5)',
+			}),
+			fontFamily: {
+				'title-serif': ['Bree Serif', 'serif'],
+				'explain-serif': ['Noto Serif Sinhala', 'serif'],
+				'nav-cursive': ['Righteous', 'cursive'],
+			},
+			content: {
+				'logo-no-icon': "url('./assets/logo/dogation-sm-logo.png')",
+				pow: "url('./assets/decorate/paw-print.png')",
+				chair: "url('./assets/decorate/beach-chair.png')",
+			},
+		},
+	},
+	screens: {
+		xs: '480px',
+		sm: '768px',
+		md: '1060px',
+	},
+	plugins: [],
+};

424d6f24b26c12e3bcfb4c73bf312435fbd9ff15
sahoooii <saho.chu@gmail.com>
2023-04-18 17:35:16 +0900

    just creating contactUs page

---
+++ src/scenes/contactUs/index.tsx
@@ -0,0 +1,12 @@
+import { SelectedPage } from '@/types/types';
+import React from 'react';
+
+type Props = {
+	setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+	return <section>ContactUs</section>;
+};
+
+export default ContactUs;
